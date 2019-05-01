aaff54921843f32163ab112ffec8ba43e3e254ea
Jade Dickinson <jadedickinson@googlemail.com>
2019-05-01 15:22:34 +0100

    Initialise project from Aphyr.com clojure tutorial

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject scratch "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[proto-repl "0.3.1"]
+                 [org.clojure/clojure "1.8.0"]])
---
+++ src/scratch/core.clj
@@ -0,0 +1,8 @@
+(ns scratch.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+(defn addition [number1 number2] (+ number1 number2))
